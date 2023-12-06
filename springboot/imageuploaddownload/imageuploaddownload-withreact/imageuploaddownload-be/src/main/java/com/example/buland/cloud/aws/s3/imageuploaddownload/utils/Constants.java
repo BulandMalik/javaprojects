649e3a25411e57be9bcf9ba128ffe2e8151d3662
BulandMalik <bulandaltaf@gmail.com>
2023-12-06 11:18:49 -0800

    backend version of storing/downloding documents with S3 and generating presigned url for download

---
+++ springboot/imageuploaddownload/imageuploaddownload-withreact/imageuploaddownload-be/src/main/java/com/example/buland/cloud/aws/s3/imageuploaddownload/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.example.buland.cloud.aws.s3.imageuploaddownload.utils;
+
+public interface Constants {
+
+    public final String DOCUMENT_CONTENT_TYPE_META_DATA_KEY = "Content-Type";
+    public final String DOCUMENT_CONTENT_LENGTH_META_DATA_KEY = "Content-Length";
+    public final String DOCUMENT_CATEGORY_META_DATA_KEY = "Document-Category";
+
+}
